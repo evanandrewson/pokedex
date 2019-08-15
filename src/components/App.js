1e66c1de36502f0f842cee2b5f3406bf6261113f
evanandrewson <evanandrewson@gmail.com>
2019-08-15 11:05:22 -0700

    added options component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import Component from './Component.js';
 import Header from './header.js';
+import Options from './Options.js';
 
 class App extends Component {
     onRender(dom) {
         const header = new Header();
         const headerDOM = header.renderDOM();
         dom.prepend(headerDOM);
+
+        const options = new Options();
+        const optionsDOM = options.renderDOM();
+        const optionsSection = dom.querySelector('.options-section');
+        optionsSection.appendChild(optionsDOM);
+
     }
     
     renderHTML() {
         return /*html*/`
-            <div></div>
+        <div>
+            <main>    
+                <section class="options-section"></section>
+            </main>
+        </div>
         `;
     }
 }
